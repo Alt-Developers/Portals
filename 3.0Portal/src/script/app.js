d76a1d3b192a96e8617ddc6d872151c36d8495b0
SEN-SENSATION <prawich.th@gmail.com>
2021-10-25 17:38:19 +0700

    First Step

---
+++ 3.0Portal/src/script/app.js
@@ -0,0 +1,39 @@
+// DOM Selection
+
+const heroTextAbstract = document.querySelector(".abstract__text");
+const heroAbstract = document.querySelector(".header__main");
+
+// Variables
+
+const abstractsImg = [
+  "./src/img/vrvalo.png",
+  "./src/img/devnews.png",
+  "./src/img/sys13.png",
+];
+
+let count = 0;
+
+// Functions
+
+const changeImg = function (imgPath) {
+  setTimeout(() => {
+    heroAbstract.style.opacity = "0";
+  }, 1000);
+  setTimeout(() => {
+    heroAbstract.style.backgroundImage = `url(${imgPath})`;
+  }, 2000);
+  setTimeout(() => {
+    heroAbstract.style.opacity = "100";
+  }, 3000);
+  console.log(`Change Image`);
+  count++;
+};
+
+// Event Handler
+
+changeImg(abstractsImg[count]);
+
+setInterval(() => {
+  count === abstractsImg.length ? (count = 0) : (count = count);
+  changeImg(abstractsImg[count]);
+}, 10000);
