15a2eb6c84870242706526e79eb5e6d9e00db7ae
Jirat Chutrakul <87472782+Jiraties@users.noreply.github.com>
2022-05-26 22:53:13 +0700

    Portals | v4 Development 26/5/2022

--- New/src/App.tsx
+++ New/src/App.tsx
@@ -9,7 +9,8 @@ function App() {
       <Header />
       <main>
         <section className="intro">
-          <h3>Who are we?</h3>
+          <h3>Introduction</h3>
+
           <p>
             SS Developers is a duo developer group developing web-applications
             and cross platform applications, who started the coding journey back
@@ -19,24 +20,52 @@ function App() {
             the frontend with frameworks like React and Flutter.
           </p>
           <div className="intro__images">
-            <a className="intro__image" href="https://github.com/Jiraties">
+            <div className="intro__image">
               <h4>
-                Jirat Chutrakul <span>Lead Frontend Developer</span>
+                Jirat Chutrakul <br />{" "}
+                <span>Lead Frontend Developer - UI/UX Designer</span>
               </h4>
+              <div className="intro__imageButtons">
+                <div>
+                  <a href="https://github.com/SEN-SENSATION">
+                    <i className="bx bxl-github"></i>
+                  </a>
+                </div>
+              </div>
               <img src="/icons/jirat.jpg" alt="a" />
-            </a>
-            <a className="intro__image" href="https://github.com/SEN-SENSATION">
+            </div>
+            <div className="intro__image">
               <h4>
-                Prawich Thawansakdivudhi <span>Lead Backend Developer</span>
+                Prawich Thawansakdivudhi <br />
+                <span>Lead Backend Developer - Database Manager</span>
               </h4>
+              <div className="intro__imageButtons">
+                <div>
+                  <a href="https://github.com/SEN-SENSATION">
+                    <i className="bx bxl-github"></i>
+                  </a>
+                </div>
+              </div>
+
               <img src="/icons/prawich.png" alt="a" />
-            </a>
+            </div>
           </div>
         </section>
 
+        <div className="separator">
+          <p></p>
+        </div>
+
         <section className="projects">
           <h3>Projects</h3>
+          <div className="projects__list">
+            <div className="projects__item">
+              <img src="/icons/Timetables1.png" alt="" />
+              <h4>Timetables</h4>
+            </div>
+          </div>
         </section>
+        <div className="separator"></div>
       </main>
     </>
   );
